2c58b6abdab52e60d3d0a1628f07c3fb63540017
raywu <25973240+raywu0123@users.noreply.github.com>
2022-03-13 22:27:12 +0800

    extract Counter class

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import mongo from './mongo.js';
-import { Range } from './models/Range.js';
 import { URLId } from './models/URLId.js';
+import Counter from './Counter.js';
 
-let rangeCounter = null;
-let counter = -1;
-
-const { URL_ID_LEN, RANGE_LEN } = process.env;
-const COUNTER_LEN = URL_ID_LEN - RANGE_LEN;
 const CHARSET = [
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
@@ -18,40 +13,25 @@ const CHARSET = [
     "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 ]
-const COUNTER_MAX = Math.pow(CHARSET.length, COUNTER_LEN);
-function encode(n, len, charset) {
-    if (n === 0) return "".padStart(len, charset[0]);
+const { URL_ID_LEN, RANGE_LEN } = process.env;
+const counter = new Counter(CHARSET, URL_ID_LEN, RANGE_LEN);
 
-    let ret = "";
-    while (n) {
-        ret += charset[n % charset.length];
-        n = Math.floor(n / charset.length);
-    }
-    return ret.padStart(len, charset[0]);
-}
-
-async function getRangeCounter() {
-    rangeCounter = (await Range.findOneAndUpdate(
-        {}, 
-        { $inc: { counter: 1 } },
-        { upsert: 1, new: 1 },
-    ).exec()).counter;
-}
 
 const app = express();
 app.use(express.json());
 app.post("/api/v1/urls", async (req, res) => {
     try {
         const { url, expireAt } = req.body;
+        try {
+            new URL(url);
+        } catch {
+            return res.status(403).json({ error: "invalid url" });
+        }
         if (new Date(expireAt) <= new Date()) 
             return res.status(403).json({ error: "expireAt is earlier than current time." });
         
-        counter += 1;
-        if (counter >= COUNTER_MAX) {
-            await getRangeCounter();
-            counter = 0;
-        }
-        const url_id = encode(rangeCounter, RANGE_LEN, CHARSET) + encode(counter, COUNTER_LEN, CHARSET);
+        await counter.step();
+        const url_id = counter.getEncoded();
         await URLId({ url, url_id, expireAt }).save();
         res.json({
             id: url_id,
@@ -67,9 +47,7 @@ app.get("/", (req, res) => {res.send("Hello World!")})
 
 async function main() {
     await mongo.connect();
-    await getRangeCounter();
-    console.log(`Range counter starting at ${rangeCounter}`);
-    console.log(`COUNTER_MAX = ${COUNTER_MAX}`);
+    await counter.init();
     const port = process.env.PORT || 5000;
     app.listen(port, () => {
         console.log(`Server started on port ${port}`);
